b2756899a1c2078203942f62bea13d4b9491acdb
Maria Rosario <mariarosariojohansson@gmail.com>
2023-06-20 23:37:02 +0200

    fetchQuestions and nextQuestion updated

--- src/app/trivia-game/trivia-game.component.ts
+++ src/app/trivia-game/trivia-game.component.ts
@@ -55,23 +55,37 @@ export class TriviaGameComponent implements OnInit {
       });
   }
 
+  shuffleArray<T>(array: T[]): T[] {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
   fetchQuestions() {
-    const url = `https://opentdb.com/api.php?amount=7`;
+    const url = `https://opentdb.com/api.php?amount=7&type=multiple`;
 
     axios
       .get(url)
       .then((response) => {
         this.questions = response.data.results;
-        this.currentQuestion = this.questions[this.currentQuestionIndex];
-        this.currentQuestion.answers = [
-          ...this.currentQuestion.incorrect_answers,
-          this.currentQuestion.correct_answer,
-        ];
-        this.currentQuestion.correctIndex =
-          this.currentQuestion.answers.indexOf(
-            this.currentQuestion.correct_answer
+
+        this.questions.forEach((question) => {
+          const allAnswers = [
+            ...question.incorrect_answers,
+            question.correct_answer,
+          ];
+          const shuffledAnswers = this.shuffleArray<string>(allAnswers);
+
+          question.answers = shuffledAnswers.map((answer) => answer);
+          question.correctIndex = shuffledAnswers.indexOf(
+            question.correct_answer
           );
-        this.currentQuestion.points = 1;
+          question.points = 1;
+        });
+
+        this.currentQuestion = this.questions[this.currentQuestionIndex];
         this.startTimer();
       })
       .catch((error) => {
@@ -100,18 +114,23 @@ export class TriviaGameComponent implements OnInit {
     }, 1000);
     console.log(this.correctAnswers);
   }
-
+  
   nextQuestion() {
     this.currentQuestionIndex++;
     if (this.currentQuestionIndex < this.questions.length) {
       this.currentQuestion = this.questions[this.currentQuestionIndex];
-      this.currentQuestion.answers = [
+
+      const allAnswers = [
         ...this.currentQuestion.incorrect_answers,
         this.currentQuestion.correct_answer,
       ];
-      this.currentQuestion.correctIndex = this.currentQuestion.answers.indexOf(
+      const shuffledAnswers = this.shuffleArray<string>(allAnswers);
+
+      this.currentQuestion.answers = shuffledAnswers;
+      this.currentQuestion.correctIndex = shuffledAnswers.indexOf(
         this.currentQuestion.correct_answer
       );
+
       this.selectedAnswer = '';
       this.timeRemaining = 31;
     } else {
